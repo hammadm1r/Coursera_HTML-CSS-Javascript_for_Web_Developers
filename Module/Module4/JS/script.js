6f185442a045c3c9cf4cdcb7666f29bd450e12d9
Hammad Mir <67120048+Hammad774@users.noreply.github.com>
2023-08-19 15:13:26 +0500

    Update script.js
    Signed-off-by: Hammad Mir <67120048+Hammad774@users.noreply.github.com>

--- Module/Module4/JS/script.js
+++ Module/Module4/JS/script.js
@@ -1,6 +1,6 @@
 (function () {
 
-    var names = ["Hammad", "Farooq", "Meer", "Rabeel", "Hamza", "Frank", "Larry", "Paula", "Laura", "Jim"];
+    var names = ["Yaakov", "John", " Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
     
     for (var i = 0; i < names.length; i++) {
     
